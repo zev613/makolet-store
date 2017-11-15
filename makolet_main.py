54904819ecb188a393876947388f4b82a1df7401
zev613 <zev123@gmail.com>
2017-11-14 20:42:23 -0500

    Added in-text comments

--- makolet_main.py
+++ makolet_main.py
@@ -1,41 +1,47 @@
+################################################################
+#	makolet_main.py, part of Zev and Harry's Final Project
+################################################################
+
 import json
 import smtplib
 import sys
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-users = {}
+users = {} #probably to remove
 
 user_logged_in = False
 current_user = {}
-user_quit = False
+user_quit = False #flag var for main while loop
 while user_quit == False:
 	print("Welcome to Makolet®, the Text-Based Store™\n")
-	print("Please select one of the following menu choices, using its corresponding keyword:\n")
+	print("Please select one of the following menu choices, using its corresponding keyword:\n") #print menu choices
 	print(" * Login for Existing Users  -  to select type: 'login' or 'log in'")
 	print(" * Create a New Account  -  to select type: 'create account' or 'create user'")
 	print(" * Quit Menu  -  to select type: 'quit' or 'exit'")
-	choice = input("Type your choice:\n")
-	valid_choices = ['login', 'log in', 'create account', 'create user', 'quit', 'exit']
-	while choice not in valid_choices:
-		print("Your choice was not one of the options listed. Please try again.")
-		choice = input("Type your choice:\n")
-	if choice == 'login' or choice == 'log in':
-		print("Go to Login Menu")
+	choice = input("Type your choice:\n") #get user choice for menu
+	valid_choices = ['login', 'log in', 'create account', 'create user', 'quit', 'exit'] #possible options to check if valid
+	while choice not in valid_choices: #if choice not valid,
+		print("Your choice was not one of the options listed. Please try again.") #have the user try again
+		choice = input("Type your choice:\n") #get new value for choice
+	if choice == 'login' or choice == 'log in': #if user picks 'login'
+		print("Go to Login Menu") #TODO: create login function, and call it here.
+		#THE LOOP WILL CONTINUE GOING UNTIL SOMETHING STOPS IT, SUCH AS INPUT. MAY NEED TO CHANGE THE LOGIC FLOW
 		#make login function, called from here.
 	elif choice == 'create account' or 'create user':
 		print("Go to Create Account Menu")
 		#make new account function, called from here.
-	elif choice == 'quit' or choice == 'exit':
+	elif choice == 'quit' or choice == 'exit': #if user wants to quit
 		print("Sorry to see you leave. Thank you for shopping with Makolet®, the Text-Based Store™\nGoodbye")
 		#Store User back into database
-		user_quit = True
-		sys.exit()
+		user_quit = True #set flag var for main loop
+		sys.exit() #quit program
 
-json_data = json.load(open('database.json'))
-users.update(json_data)
+json_data = json.load(open('database.json')) #load user database from json file
+users.update(json_data) #update empty user dictionary from the json file
 
 class User():
+	"""Makes a new user with the specified attributes"""
 	def __init__(self, first_name, last_name, email, password):
 		self.first_name = first_name
 		self.last_name = last_name
@@ -56,11 +62,11 @@ class User():
 	def get_password(self):
 		return self.password
 	
-zseltzer = User('Zev', 'Seltzer', 'zev123@gmail.com', "Zev'sPassword123")
+zseltzer = User('Zev', 'Seltzer', 'zev123@gmail.com', "Zev'sPassword123") #create a new user, Zev
 
 print('zseltzer.get_first_name:', zseltzer.get_first_name())
 	
 	
 
-def user_login():
-	pass
+def user_login(): #TODO: Complete this user_login function
+	print('Welcome to the Login Page')
